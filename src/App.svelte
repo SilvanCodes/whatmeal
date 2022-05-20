8d44320956b00165d2aad09f1e2168ff028ac453
Silvan Buedenbender <silvancodes@gmail.com>
2022-05-20 22:48:36 +0200

    Rework visual presentation
    Use values from the scales throughout the design and adopt ratio.

--- src/App.svelte
+++ src/App.svelte
@@ -23,46 +23,45 @@
   onMount(getRandomMeal);
 </script>
 
-<div class="elc-cover" style="--cover-padding: var(--s1)">
-  <div class="elc-stack" style="--stack-margin: calc(var(--s2) * 0.75)">
-    <div class="elc-stack" style="--stack-margin: calc(var(--s1))">
-      <div class="elc-stack elc-center">
-        <h2 style="font-size: xx-large;">Wie wäre es mit</h2>
-        <h1 style="font-size: xxx-large;">
-          {#await suggestion}
-            <span class="pulse">?</span>
-          {:then name}
-            <span style="word-break: break-word;">{name}</span>
-          {/await}
-        </h1>
-      </div>
-      <div class="elc-center">
-        <button
-          style="font-size: calc(var(--s0) * 1.1);"
-          class="pulse-on-click"
-          on:click={getRandomMeal}>Anderer Vorschlag</button
+<div class="elc-cover" style="--cover-padding: var(--s3)">
+  <div class="dummy-for-cover-layout" />
+
+  <div class="elc-center elc-stack" style="--stack-margin: var(--s4)">
+    <h2 class="text-align:center">Wie wäre es mit</h2>
+
+    <h1 class="text-align:center">
+      {#await suggestion}
+        <span class="pulse">?</span>
+      {:then name}
+        <span class="word-break:break-word">{name}</span>
+      {/await}
+    </h1>
+
+    <button
+      class="pulse-on-click font-size:base-plus primary"
+      on:click={getRandomMeal}>Anderer Vorschlag</button
+    >
+  </div>
+
+  <div class="elc-center">
+    <div class="elc-stack">
+      <form on:submit|preventDefault={addMeal} class="elc-box elc-stack">
+        <input
+          bind:this={addMealInput}
+          bind:value={addMealInputValue}
+          type="text"
+          placeholder="Was gab es gestern?"
+          class="text-align:center"
+        />
+        <button class="pulse-on-click secondary"
+          >Eigenen Vorschlag hinzufügen</button
         >
-      </div>
-    </div>
+      </form>
 
-    <div class="elc-center">
-      <div class="elc-stack">
-        <form on:submit|preventDefault={addMeal} class="elc-box elc-stack">
-          <input
-            bind:this={addMealInput}
-            bind:value={addMealInputValue}
-            type="text"
-            placeholder="Was gab es gestern?"
-            class="text-align:center"
-          />
-          <button class="pulse-on-click">Eigenen Vorschlag hinzufügen</button>
-        </form>
-
-        <div class="elc-box elc-stack">
-          <button on:click={deleteMeal} class="pulse-on-click"
-            >Diesen Vorschlag löschen</button
-          >
-        </div>
+      <div class="elc-box">
+        <button on:click={deleteMeal} class="pulse-on-click secondary"
+          >Angezeigten Vorschlag löschen</button
+        >
       </div>
     </div>
   </div>
